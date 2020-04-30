907a68a0209ae754a16e9e33149e1b9bdc38d5b1
Karikari1234 <42123922+Karikari1234@users.noreply.github.com>
2020-04-30 16:49:04 +0600

    Render messages and link to user location on the webpage

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ const express = require('express')
 const path = require('path')
 const http = require('http')
 const socketio = require('socket.io')
+const { generateMessage, generateLocationMessage } = require('./utils/message.js')
 
 const app = express()
 
@@ -12,11 +13,22 @@ const io = socketio(server)
 
 io.on('connection', (socket) => {
     console.log('New User Connected')
-    socket.emit('message', 'Welcome!')
+    socket.emit('message', generateMessage('Welcome!'))
 
-    socket.on('increment', () => {
-        count++
-        io.emit('countUpdated', count)
+    socket.broadcast.emit('enterMessage', 'A new User has joined the chat')
+
+    socket.on('sendMessage', (message, callback) => {
+        io.emit('message', generateMessage(message))
+        callback('delivered')
+    })
+
+    socket.on('disconnect', () => {
+        io.emit('exitMessage', 'A user has left the chat')
+    })
+
+    socket.on('location', (location, callback) => {
+        socket.broadcast.emit('locationMessage', generateLocationMessage(location))
+        callback('Shared!')
     })
 
 })
